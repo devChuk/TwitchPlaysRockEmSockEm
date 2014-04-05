a1ac55a9ff16b032fefc75db9f04a923b59a676d
Brian Chuk <brian_chuk@yahoo.com>
2014-04-05 13:23:38 -0400

    yay we can talk!

---
+++ Test_Code/Arduino/send2Arduino/send2Arduino.ino
@@ -0,0 +1,34 @@
+//This code will enable the user to turn an LED attached to an Arduino on and off with 1 and 0 respectively.
+
+int ledPin = 13;
+
+void setup() {
+  //Create Serial Object
+  Serial.begin(9600);
+  //Set pin of led to output
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  //Have the arduino wait to receive input
+  while (Serial.available() == 0);
+  
+  //Read the Input
+  int val = Serial.read() - '0';
+  
+  if (val == 1) 
+  {
+    Serial.println("Led is On"); 
+    digitalWrite(ledPin, HIGH);
+  }
+  else if (val == 0)
+  {
+    Serial.println("Led is Off");
+    digitalWrite(ledPin, LOW);
+  }
+  else
+  {
+    Serial.println("Invalid!");
+  }
+  Serial.flush(); 
+}
