67f9477ce32571433055a074fc28485afcfaffd1
Brian Chuk <brian_chuk@yahoo.com>
2014-06-01 17:09:28 -0400

    planning out databasing stuffs

---
+++ mongostuff/databasetest.py
@@ -0,0 +1,23 @@
+import pymongo
+from pymongo import MongoClient
+
+
+client = MongoClient()
+db = client['test-database']
+collection = db.test_collection
+#The thing is that MongoDB does not like documents which grow, like chat pages. 
+#MongoDB always stores documents in a contiguous section in the database files. This makes reading much simpler and faster, 
+#because no seeking on the hard-drive needs to be done to read a single document.
+
+#That means when a document grows so large that it would overlap the space of 
+#the next document in the file, that document needs to be removed and new hard-drive 
+#space needs to be allocated to store it. This is an expensive operation. When your documents 
+#grow all the time, this will happen very frequently and write-performance will suffer.
+
+#So which would be faster, having two documents for each robot or one large document for both robots?
+#If we have two documents, writing will probably be slower than reading. If we have one, reading will be slower than writing.
+#Long story short, we're probably going to have lag with Python no matter what happens. Better to have two documents so 
+#programming the reading code will be easier.
+
+#There will be two documents. Each document is for each robot.
+
